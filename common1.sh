ca6769365828ee981b668d93b0d9f889d1387d8d
maxocean <39007967+haohaoget@users.noreply.github.com>
2021-03-11 13:06:28 +0800

    Update common1.sh

--- common1.sh
+++ common1.sh
@@ -4,20 +4,17 @@ Diy_n1() {
 KERNEL_VERSION="5.4.101-flippy-54+o"
 VERSION="5.4.101"
 mkdir opt
-sudo chmod  -R 777 /opt
-tar -xzvf mk_openwrt_src_20210304.tar.gz -C /opt
+chmod  -R 777 opt
+tar -xzvf mk_openwrt_src_20210304.tar.gz -C opt
 cp openwrt/bin/targets/armvirt/*/*.tar.gz opt/openwrt    
 cd opt
 mkdir imgs
 chmod  -R 777 imgs
-ls
 cd imgs
 pathimg=$PWD
 wget https://github.com/haohaoget/N1/releases/download/${VERSION}/Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
 unxz Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
-ls
 cd ../
-ls
 cd openwrt
 sed -i 's//opt/imgs/${pathimg}/g' mk_s905d_n1.sh
 chmod +x mk_s905d_n1.sh
