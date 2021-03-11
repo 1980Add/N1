708f2d154f6ecda182b8e55f5bb8fb557d3a0e40
maxocean <39007967+haohaoget@users.noreply.github.com>
2021-03-11 15:41:35 +0800

    Update common1.sh

--- common1.sh
+++ common1.sh
@@ -10,13 +10,14 @@ cp openwrt/bin/targets/armvirt/*/*.tar.gz opt/openwrt
 cd opt
 mkdir imgs
 chmod  -R 777 imgs
+echo "PATH2=${GITHUB_WORKSPACE}/opt/imgs" >> $GITHUB_ENV
 cd imgs
 pathimg=$PWD
 wget https://github.com/haohaoget/N1/releases/download/${VERSION}/Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
 unxz Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
 cd ../
 cd openwrt
-sed -i 's//opt/imgs/${pathimg}/g' mk_s905d_n1.sh
+sed -i "s%/opt/imgs%${pathimg}%g" mk_s905d_n1.sh
 chmod +x mk_s905d_n1.sh
 sudo ./mk_s905d_n1.sh
 sleep 20
