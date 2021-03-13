f9979d519a2ec913ce1bc4e75e04d56cfe669b84
1980Add <68543817+1980Add@users.noreply.github.com>
2021-03-13 13:51:40 +0800

    Update common1.sh

--- common1.sh
+++ common1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 Diy_n1() {
-KERNEL_VERSION="5.4.101-flippy-54+o"
-VERSION="5.4.101"
+KERNEL_VERSION="5.4.105-flippy-55+o"
+VERSION="5.4.105"
 mkdir opt
 chmod  -R 777 opt
-tar -xzvf mk_openwrt_src_20210304.tar.gz -C opt
+tar -xzvf mk_openwrt_src_20210312.tar.gz -C opt
 cp openwrt/bin/targets/armvirt/*/*.tar.gz opt/openwrt    
 cd opt
 mkdir imgs
@@ -13,7 +13,7 @@ chmod  -R 777 imgs
 echo "PATH2=${GITHUB_WORKSPACE}/opt/imgs" >> $GITHUB_ENV
 cd imgs
 pathimg=$PWD
-wget https://github.com/haohaoget/N1/releases/download/${VERSION}/Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
+wget https://github.com/1980Add/N1/releases/download/${VERSION}/Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
 unxz Armbian_20.10_Aml-s9xxx_buster_${KERNEL_VERSION}.img.xz
 cd ../
 cd openwrt
